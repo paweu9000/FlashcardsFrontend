3a1cad061d93921ec6e937e01c2dd746b06380cc
paweu9000 <wolfenorrin5@gmail.com>
2023-03-14 18:10:42 +0100

    initial

---
+++ flashfront/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Router, Route } from 'svelte-navigator';
+    import Home from './routes/Home.svelte';
+    import RouteTwo from './routes/RouteTwo.svelte';
+</script>
+
+<main>
+	<Router>
+		<Route path="/" component={Home} />
+		<Route path="/2" component={RouteTwo} />
+	</Router>
+</main>
+
+
---
+++ flashfront/src/components/Navbar.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+      <li><a href="/">Home<href></li>
+      <li><a href="/about">Page two<href></li>
+    </ul>
+</nav>
