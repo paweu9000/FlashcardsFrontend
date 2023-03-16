cb7a61e15451abfea3b7c92d1a1ceabfe06f894b
paweu9000 <wolfenorrin5@gmail.com>
2023-03-16 18:51:40 +0100

    register setup api

--- flashfront/src/routes/RegisterParent.svelte
+++ flashfront/src/routes/RegisterParent.svelte
@@ -1,9 +1,23 @@
 <script>
     import Register from './Register.svelte';
     import Navbar from '../components/Navbar.svelte';
-  
+    import axios from 'axios';
+
+    const apiAdress = "http://localhost:3000/api/auth/register";
+
     const handleRegistration = (formData) => {
-      console.log(formData);
+      axios.post(apiAdress, {
+          login: formData.login,
+          username: formData.username,
+          email: formData.email,
+          password: formData.password
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
     };
 </script>
   
