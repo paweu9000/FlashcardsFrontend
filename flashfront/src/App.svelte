5e76a707f4ba44153b510a455951850ae165c61d
paweu9000 <wolfenorrin5@gmail.com>
2023-03-19 19:28:56 +0100

    setup server listener

--- flashfront/src/App.svelte
+++ flashfront/src/App.svelte
@@ -3,6 +3,7 @@
     import Home from './routes/Home.svelte';
     import RouteTwo from './routes/RouteTwo.svelte';
 	import RegisterParent from './routes/RegisterParent.svelte';
+	import EmailVerification from './routes/EmailVerification.svelte';
 </script>
 
 <main>
@@ -10,6 +11,7 @@
 		<Route path="/" component={Home} />
 		<Route path="/2" component={RouteTwo} />
 		<Route path="/register" component={RegisterParent}/>
+		<Route path="/verify/:token" component={EmailVerification}/>
 	</Router>
 </main>
 
