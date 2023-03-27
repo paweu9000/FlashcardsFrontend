d3d941408fbe7f7a0811a65a271e527a010f899b
paweu9000 <wolfenorrin5@gmail.com>
2023-03-27 19:15:30 +0200

    initial style for add cards

--- flashfront/src/routes/CreateCollection.svelte
+++ flashfront/src/routes/CreateCollection.svelte
@@ -71,7 +71,11 @@
     </div>
     {#each flashcards as flashcard, i}
         <div class="flashcard">
-            <button class="close-button" on:click={() => deleteFlashcard(i)}>X</button>
+            <button class="ui red button" on:click={() => deleteFlashcard(i)}>
+                <i class="archive icon"></i>
+            </button>
+            <br>
+            <br>
             <label>Term:
                 <input minlength="1" bind:value={flashcard.side} placeholder="{flashcard.side}">
             </label>
@@ -81,14 +85,22 @@
         </div>
     {/each}
     <div>
-        <button class="button-29" on:click={addFlashcard}>Add Flashcard</button>
-        {#if flashcards.length > 1}
-            <button class="button-29" on:click={saveFlashcards}>Save Flashcards</button>
+        <button class="ui active button" on:click={addFlashcard}>
+            <i class="plus circle icon"></i>
+            Add Flashcard
+        </button>
+        {#if flashcards.length > 1 && title.length >= 1}
+            <button class="ui active button" on:click={saveFlashcards}>
+                <i class="save icon"></i>
+                Save Flashcards
+            </button>
         {/if}
     </div>
 </div>
+<br>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
     .flashcards-container {
         display: flex;
         flex-direction: column;
@@ -99,7 +111,7 @@
         margin: 10px;
         padding: 20px;
         width: 600px;
-        height: 170px;
+        height: 230px;
         border-radius: 10px;
         box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
         background-color: #fff;
@@ -109,8 +121,8 @@
     }
     label {
         font-size: 20px;
-        font-weight: bold;
         margin-bottom: 10px;
+        font-family: 'Roboto', sans-serif;
     }
 
     input {
@@ -125,61 +137,5 @@
         outline: none;
         box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.2);
         border-color: blue;
-    }
-    
-    .close-button {
-        width: 20px;
-        height: 20px;
-        border: none;
-        background-color: transparent;
-        font-size: 16px;
-        font-weight: bold;
-        color: black;
-        cursor: pointer;
-    }
-
-    .button-29 {
-        align-items: center;
-        appearance: none;
-        background-image: radial-gradient(100% 100% at 100% 0, #5adaff 0, #5468ff 100%);
-        border: 0;
-        border-radius: 6px;
-        box-shadow: rgba(45, 35, 66, .4) 0 2px 4px,rgba(45, 35, 66, .3) 0 7px 13px -3px,rgba(58, 65, 111, .5) 0 -3px 0 inset;
-        box-sizing: border-box;
-        color: #fff;
-        cursor: pointer;
-        display: inline-flex;
-        font-family: "JetBrains Mono",monospace;
-        height: 48px;
-        justify-content: center;
-        line-height: 1;
-        list-style: none;
-        overflow: hidden;
-        padding-left: 16px;
-        padding-right: 16px;
-        position: relative;
-        text-align: left;
-        text-decoration: none;
-        transition: box-shadow .15s,transform .15s;
-        user-select: none;
-        -webkit-user-select: none;
-        touch-action: manipulation;
-        white-space: nowrap;
-        will-change: box-shadow,transform;
-        font-size: 18px;
-    }
-
-    .button-29:focus {
-        box-shadow: #3c4fe0 0 0 0 1.5px inset, rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
-    }
-
-    .button-29:hover {
-        box-shadow: rgba(45, 35, 66, .4) 0 4px 8px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
-        transform: translateY(-2px);
-    }
-
-    .button-29:active {
-        box-shadow: #3c4fe0 0 3px 7px inset;
-        transform: translateY(2px);
     }
 </style>
