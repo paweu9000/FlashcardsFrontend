4f3efd41fa962deda6a5085518e9eea8a5e22145
paweu9000 <wolfenorrin5@gmail.com>
2023-03-28 14:27:04 +0200

    initial error component and implementation in register

--- flashfront/src/routes/RegisterParent.svelte
+++ flashfront/src/routes/RegisterParent.svelte
@@ -5,12 +5,15 @@
     import axios from 'axios';
     import SuccessRegister from '../components/SuccessRegister.svelte';
     import {useNavigate} from 'svelte-navigator';
+    import Error from '../components/Error.svelte';
 
     const navigate = useNavigate();
 
     const apiAdress = "http://localhost:3000/api/auth/register";
 
     let verified = false;
+
+    let errors = [];
 
     onMount(async () => {
       const storedValue = localStorage.getItem('verified');
@@ -19,28 +22,44 @@
       }
     })
 
+    function formValid(formData) {
+      errors = [];
+      if(formData.login.length > 30 || formData.login.length < 2) errors.push("Login length must be between 2 and 30 characters.");
+      if(formData.username.length > 30 || formData.username.length < 2) errors.push("Username length must be between 2 and 30 characters.");
+      if(formData.password.length < 6) errors.push("Password length must be at least 6 characters.");
+    }
+
     const handleRegistration = (formData) => {
-      axios.post(apiAdress, {
+      formValid(formData);
+      if (errors.length == 0) {
+        axios.post(apiAdress, {
           login: formData.login,
           username: formData.username,
           email: formData.email,
           password: formData.password
-      })
-      .then((response) => {
-        console.log(response);
-        if (response.status == 201) {
-          verified = true;
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+        })
+        .then((response) => {
+          if (response.status == 201) {
+            verified = true;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      }
     };
 </script>
 
 <Navbar />
 {#if verified}
   <SuccessRegister />
+{/if}
+{#if !verified && errors.length > 0}
+  {#each errors as error}
+  <div class="container">
+    <Error errorValue={error}/>
+  </div>
+  {/each }
 {/if}
 <Register onSubmit={handleRegistration} />
 
