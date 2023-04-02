c4ac9ce529893bdc7628bf732846fc6006783e29
paweu9000 <wolfenorrin5@gmail.com>
2023-04-02 17:39:20 +0200

    initial searchbar

---
+++ flashfront/src/components/SearchBar.svelte
@@ -0,0 +1,47 @@
+<script>
+    import axios from "axios";
+    import { useNavigate } from "svelte-navigator";
+
+    const navigate = useNavigate();
+
+    const collectionApi = "http://localhost:3000/api/collection/search/";
+
+    let input = "";
+
+    let content = [];
+
+    function search() {
+        axios.get(collectionApi + input, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        })
+        .then(response => {
+            content = response.data;
+        }).catch(error => {
+            console.log(error);
+        })
+
+        jQuery('.ui.search').search({
+            source: content,
+            onSelect: function(result, response) {
+                navigate(`/flashcards/${result.id}`, {replace: true, reload: true});
+                location.reload();
+            }
+        })
+    }
+
+</script>
+
+<div class="ui search">
+    <div class="ui icon input">
+      <input class="prompt" id="input" type="text" placeholder="Search" bind:value={input} on:input={search}>
+      <i class="search icon"></i>
+    </div>
+    <div class="results">
+    </div>
+</div>
+
+<style>
+    .ui.search {
+        margin-left: 20%;
+    }
+</style>
