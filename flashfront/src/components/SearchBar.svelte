9e3f780dd5119a173cd6a8366ec49253a44c05aa
paweu9000 <wolfenorrin5@gmail.com>
2023-04-03 22:22:31 +0200

    searchbar working fix

--- flashfront/src/components/SearchBar.svelte
+++ flashfront/src/components/SearchBar.svelte
@@ -5,28 +5,67 @@
     const navigate = useNavigate();
 
     const collectionApi = "http://localhost:3000/api/collection/search/";
+    const userApi = "http://localhost:3000/api/user/search/";
 
     let input = "";
+
+    let isCollection = true;
 
     let content = [];
 
     function search() {
-        axios.get(collectionApi + input, {
+        if (isCollection) {
+            axios.get(collectionApi + input, {
             headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-        })
-        .then(response => {
-            content = response.data;
-        }).catch(error => {
-            console.log(error);
-        })
+            })
+            .then(response => {
+                content = response.data;
+                console.log(content);
+                jQuery('.ui.search').search({
+                source: content,
+                onSelect: function(result, response) {
+                    navigate(`/flashcards/${result.id}`, {replace: true, reload: true});
+                    location.reload();
+                }
+            })
+            }).catch(error => {
+                console.log(error);
+            })
+        } else {
+            axios.get(userApi + input, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+            })
+            .then(response => {
+                content = response.data;
+                console.log(content);
+                jQuery('.ui.search').search({
+                    source: content.map(item => {
+                    return {
+                        title: item.username,
+                        id: item.id
+                    }
+                }),
+                searchFields: ['title'],
+                fields: {
+                    title: 'title',
+                    id: 'id'
+                },
+                minCharacters: 0,
+                onSelect: function(result, response) {
+                    console.log(result.id);
+                    // navigate(`/flashcards/${result.id}`, {replace: true, reload: true});
+                    // location.reload();
+                }
+            })
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
 
-        jQuery('.ui.search').search({
-            source: content,
-            onSelect: function(result, response) {
-                navigate(`/flashcards/${result.id}`, {replace: true, reload: true});
-                location.reload();
-            }
-        })
+    function slide() {
+        input = "";
+        isCollection = !isCollection;
     }
 
 </script>
@@ -34,7 +73,7 @@
 <div class="ui search">
     <i class="copy icon"></i>
     <div class="ui toggle checkbox">
-        <input type="checkbox" name="public">
+        <input type="checkbox" name="public" id="check_id" on:click={slide}>
         <label></label>
     </div>
     <i class="user icon"></i>
